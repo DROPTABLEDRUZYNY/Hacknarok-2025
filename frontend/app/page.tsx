0290791cdb2f4f18910655f4e955e96c8e9a01f9
MrNtex <ntexyt@gmail.com>
2025-04-12 14:38:10 +0200

    feat: Add background gradient animation and new keyframe animations to global styles

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
 export default function Page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 ">
       <main className="flex flex-col row-start-2 items-center">
-        <Link href={"/activity"}>
-          <button
-            className="shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest 
+        
+        <div className="absolute top-0 left-0 w-full h-full">
+          <BackgroundGradientAnimation />
+        </div>
+        <div className="relative z-10">
+          <Link href={"/activity"}>
+            <button className="shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest 
         uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 
-        transition duration-200"
-          >
-            {" "}
-            Let's LiveActive!{" "}
-          </button>
-        </Link>
+        transition duration-200">Let's LiveActive!</button>
+          </Link>
+        </div>
       </main>
     </div>
   );
